c41e70bee384fbc71fd591059a112284dfedf71e
Bberkayyy <bberkayoguz@gmail.com>
2023-12-29 14:29:38 +0300

    reservation class, car reservation process and ui added

--- Core/UdemyCarBook.Domain/Entities/Location.cs
+++ Core/UdemyCarBook.Domain/Entities/Location.cs
@@ -11,4 +11,6 @@ public class Location
     public int Id { get; set; }
     public string Name { get; set; }
     public List<RentACar> RentACars { get; set; }
+    public List<Reservation> PickUpReservation { get; set; }
+    public List<Reservation> DropOffReservation { get; set; }
 }
---
+++ Frontends/UdemyCarBook.WebUI/Controllers/ReservationController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Newtonsoft.Json;
+using System.Net.Http;
+using UdemyCarBook.DTOs.LocationDTOs;
+
+namespace UdemyCarBook.WebUI.Controllers;
+
+public class ReservationController : Controller
+{
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public ReservationController(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+    [HttpGet]
+    public async Task<IActionResult> Index()
+    {
+        ViewBag.v1 = "Araç Kiralama";
+        ViewBag.v2 = "Araç Rezervasyon Formu";
+        var client = _httpClientFactory.CreateClient();
+        var responseMessage = await client.GetAsync("https://localhost:7041/api/Locations");
+
+        var jsonData = await responseMessage.Content.ReadAsStringAsync();
+        var values = JsonConvert.DeserializeObject<List<ResultLocationDTO>>(jsonData);
+        List<SelectListItem> values2 = (from x in values
+                                        select new SelectListItem
+                                        {
+                                            Text = x.Name,
+                                            Value = x.Id.ToString()
+                                        }).ToList();
+        ViewBag.v = values2;
+        return View();
+    }
+}
